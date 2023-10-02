c0553beee3e2b3a5e0801482f36bee37924a9717
RomeyKung <88416990+RomeyKung@users.noreply.github.com>
2023-10-02 23:43:13 +0700

    fixfirebase

--- FrontEndMobile/FirebaseConfig.ts
+++ FrontEndMobile/FirebaseConfig.ts
@@ -1,13 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-import {getAuth, initializeAuth} from "firebase/auth";
-import {getFirestore} from "firebase/firestore";
-// import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import {
+  getAuth,
+  initializeAuth,
+  getReactNativePersistence,
+} from "firebase/auth";
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -18,12 +22,15 @@ const firebaseConfig = {
   storageBucket: "tutorflix-c64b4.appspot.com",
   messagingSenderId: "810840685231",
   appId: "1:810840685231:web:506576d8f8032f8c13f25a",
-  measurementId: "G-EPZTQ0WG4D"
+  measurementId: "G-EPZTQ0WG4D",
 };
 
 // Initialize Firebase คือ การเชื่อมต่อกับ Firebase โดยใช้ค่า config ที่ได้จากข้างบน
 export const FIREBASE_APP = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(FIREBASE_APP);
+const auth = initializeAuth(FIREBASE_APP, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+});
 
 // สร้างตัวแปรเพื่อเรียกใช้งาน Firebase Authentication และ Firebase Firestore
 export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
--- FrontEndMobile/app/Navigation/MainStack.tsx
+++ FrontEndMobile/app/Navigation/MainStack.tsx
@@ -1,17 +1,57 @@
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import React from "react";
-import { View, Text } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { View, Text, StyleSheet, Image } from "react-native";
 
 import { HomeScreen, List } from "../screens/Index";
 
 const Tab = createBottomTabNavigator();
 const MainStack = () => {
   return (
-    <Tab.Navigator initialRouteName="Home">
-      <Tab.Screen name="Home" component={HomeScreen} />
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        // tabBar
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          position: "absolute",
+          bottom: 25,
+          left: 20,
+          right: 20,
+          elevation: 0,
+          backgroundColor: "#ffffff",
+          borderRadius: 15,
+          height: 90,
+          ...styles.shadow,
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View>
+              <Image source={require("../../assets/icons/logo.png")} style={{width:60, height:60}}/>
+            </View>
+          ),
+        }}
+      />
       <Tab.Screen name="List" component={List} />
     </Tab.Navigator>
   );
 };
 
 export default MainStack;
+
+const styles = StyleSheet.create({
+  shadow: {
+    shadowColor: "#7f5df0",
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.5,
+    elevation: 5,
+  },
+});
