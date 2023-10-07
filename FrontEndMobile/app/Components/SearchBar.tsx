5f62f829c00a1ff093b5f58df02c72b35c313b17
[Ta1oZ] <[pond.tnd@gmail.com]>
2023-10-07 22:29:31 +0700

    Fix

--- FrontEndMobile/app/Components/SearchBar.tsx
+++ FrontEndMobile/app/Components/SearchBar.tsx
@@ -1,42 +1,30 @@
 import React, { useState } from "react";
 import { Feather, Entypo } from "@expo/vector-icons";
-import { View, Text, StyleSheet, Button, TextInput, Keyboard } from "react-native";
+import { View, Text, StyleSheet, Button, TextInput, Keyboard, TouchableWithoutFeedback, TouchableOpacity } from "react-native";
 
 
 const SearchBar = () => {
     {
-        const [clicked, setClicked] = useState(false);
         const [searchPhrase, setSearchPhrase] = useState("");
+        const [isFocus, setIsFocus] = useState(false)
         return (
-            <View style={styles.container}>
-                <View style={clicked ? styles.searchBar_clicked : styles.searchBar_unclicked}>
-                    <Feather name="search" size={20} color="black" style={{ marginLeft: 1 }} />
-                    <TextInput style={styles.input}
-                        placeholder="Search"
-                        value={searchPhrase}
-                        onChangeText={setSearchPhrase}
-                        onFocus={() => {
-                            setClicked(true);
-                        }}
-                    />
-                    {clicked && (
-                        <Entypo name="cross" size={20} color="black" style={{ padding: 1 }} onPress={() => {
-                            setSearchPhrase("")
-                        }} />
-                    )}
-                </View>
-                {clicked && (
-                    <View>
-                        <Button
-                            title="Cancel"
-                            onPress={() => {
-                                Keyboard.dismiss();
-                                setClicked(false);
-                            }}
-                        ></Button>
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+                <View style={styles.container}>
+                    <View style={styles.searchBar_unclicked}>
+                        <View style={{ flexDirection: "row", alignItems: "center" }}>
+                            <Feather name="search" size={20} color="black" />
+                            <TextInput style={styles.input}
+                                placeholder="Search"
+                                value={searchPhrase}
+                                onChangeText={setSearchPhrase}
+                            />
+                        </View>
+                        {searchPhrase !== "" ? <TouchableOpacity onPress={ () => setSearchPhrase("")}>
+                            <Entypo name="cross" size={20} color="black" />
+                        </TouchableOpacity> : null}
                     </View>
-                )}
-            </View>
+                </View>
+            </TouchableWithoutFeedback>
         )
     }
 }
@@ -52,25 +40,18 @@ const styles = StyleSheet.create({
 
     },
     searchBar_unclicked: {
+        marginLeft: 20,
         padding: 10,
         flexDirection: "row",
-        width: "95%",
+        width: "90%",
         backgroundColor: "#d9dbda",
         borderRadius: 15,
         alignItems: "center",
-    },
-    searchBar_clicked: {
-        padding: 10,
-        flexDirection: "row",
-        width: "80%",
-        backgroundColor: "#d9dbda",
-        borderRadius: 15,
-        alignItems: "center",
-        justifyContent: "space-evenly",
+        justifyContent: "space-between"
     },
     input: {
         fontSize: 20,
         marginLeft: 10,
-        width: "90%",
+        width: "85%",
     },
 });
